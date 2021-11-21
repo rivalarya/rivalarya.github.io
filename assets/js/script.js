5010f3e0bf2e83f887a181d3a97ea9e9da04a0ca
Rival <rifall.osd26@gmail.com>
2021-11-22 00:14:05 +0700

    menambahkan plugin lightbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,12 +19,23 @@ if (row != null) {
                 const figure = document.createElement("FIGURE");
                 div.appendChild(figure)
 
+                // tag a untuk lightbox gambar
+                const alighb = document.createElement("A");
+                const href = document.createAttribute("href");
+                href.value = `assets/img/portofolio/${data[i].img}`;
+                alighb.setAttributeNode(href)
+                const data_fslightbox = document.createAttribute("data-fslightbox");
+                data_fslightbox.value = "gallery";
+                alighb.setAttributeNode(data_fslightbox)
+
                 // tag img untuk profil
                 const img = document.createElement("IMG");
                 const src = document.createAttribute("src");
                 src.value = `assets/img/portofolio/${data[i].img}`;
                 img.setAttributeNode(src)
-                figure.appendChild(img)
+                alighb.appendChild(img)
+                figure.appendChild(alighb) //kemudian masukan tag a ke tag figure
+
 
                 // tag h2 untuk nama portofolio
                 const h2 = document.createElement("H2");
@@ -33,20 +44,22 @@ if (row != null) {
                 h2.appendChild(judul);
                 kelasJudul.value = "portofolio-title";
                 h2.setAttributeNode(kelasJudul)
-                div.appendChild(h2)
+                div.appendChild(h2) //kemudian masukan tag h2 ke tag div utama
 
                 // tag div dengan class portofolio - desc
                 const divDesc = document.createElement("DIV");
                 const kelasDivDesc = document.createAttribute("class");
                 kelasDivDesc.value = "portofolio-desc";
                 divDesc.setAttributeNode(kelasDivDesc)
-                div.appendChild(divDesc)
+                div.appendChild(divDesc) //kemudian masukan tag div deskripsi ke tag div utama
 
                 // tag p untuk deskripsi
                 const p = document.createElement("P");
                 const desc = document.createTextNode(`${data[i].deskripsi}.`);
                 p.appendChild(desc);
-                divDesc.appendChild(p);
+                divDesc.appendChild(p); //kemudian masukan tag p ke tag div deskripsi
+
+                refreshFsLightbox(); // fungsi agar lightbox mendetect gambar yg baru di append
             }
         }
     }
